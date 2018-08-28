1b428027498600fb57d38f452e2767a1afe52dab
Justine Bricknell <justinegayton@gmail.com>
2018-08-28 20:32:14 +0000

    updated scraper class

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,11 @@
-require_relative "./colorado-fourteeners/version"
-require_relative "./colorado-fourteeners/cli"
-require_relative "./colorado-fourteeners/scraper"
-require_relative "./colorado-fourteeners/mountain"
-require 'open-uri'
 require 'pry'
 require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/colorado-fourteeners/version'
+require_relative '../lib/colorado-fourteeners/cli'
+require_relative '../lib/colorado-fourteeners/scraper'
+require_relative '../lib/colorado-fourteeners/mountain'
 
 
 #this file acts as envs and this can require all the dependencies and the other files can call on this one. 
--- lib/colorado-fourteeners/scraper.rb
+++ lib/colorado-fourteeners/scraper.rb
@@ -1,2 +1,18 @@
-class ColoradoFourteeners::Scraper
+require 'open-uri'
+require 'nokogiri'
+
+ class Scraper
+   def get_page
+    Nokogiri::HTML(open("https://www.14ers.com/photos/photos_14ers1.php"))
+    end
+    
+  def scrape_mountain_list 
+     self.get_page.css("td a href img a") #.attr("a")
+     #("td a").attr("href").value
+  end
+
 end
+
+s = Scraper.new
+s.scrape_mountain_list
+
