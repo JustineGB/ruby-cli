3b72b65375230126a18f513c59f7a37b9a8a372f
Justine Bricknell <justinegayton@gmail.com>
2018-08-28 16:43:21 +0000

    test commit

---
+++ lib/colorado-fourteeners.rb
@@ -0,0 +1,11 @@
+module ColoradoFourteeners 
+end 
+
+require_relative "./mountains/version"
+require_relative "./mountains/cli"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+
+#this file acts as envs and this can require all the dependencies and the other files can call on this one. 
--- lib/mountains/version.rb
+++
@@ -1,4 +0,0 @@
- module Mountains
-    VERSION = "0.1.0"
-  end
-end
